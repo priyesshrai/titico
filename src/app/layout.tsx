790b04caeb3aef05900f0d402290d16b42126f93
priyesshrai <priyeshrai369@gmail.com>
2026-01-27 15:49:36 +0530

    project initial setup done.

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Poppins, Montserrat_Alternates } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat_Alternates({
+  variable: "--font-montserrat",
+  weight: ["100", "200", "300", "400", "500", "500", "600", "700", "800"]
+});
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "500", "600", "700", "800"]
+});
+
+export const metadata: Metadata = {
+  title: "Titico Textiles",
+  description: "Titico Textiles is one of the largest exporter of silk fabric in varanasi",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${montserrat.variable} ${poppins.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <h1 className="font-mono">Hello</h1>
+  );
+}
